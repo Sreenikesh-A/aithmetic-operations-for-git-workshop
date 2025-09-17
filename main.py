82b581887a3f71df47f53570cdeea5bceaf17972
sreenikesh <24F2002367@ds.study.iitm.ac.in>
2025-09-17 23:40:44 +0530

    Use arithmetic modules in main.py

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print("arithmetic operators")
+from sum import add
+from difference import subtract
+from product import multiply
+
+def main():
+    print("2 + 3 =", add(2, 3))
+    print("5 - 2 =", subtract(5, 2))
+    print("3 * 4 =", multiply(3, 4))
+
+if _name_ == "_main_":
+    main()
